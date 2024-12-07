892244c44dff4982dc153a66c9e336322f16fc83
Foster Z <fosterunlimited0123@gmail.com>
2024-12-07 15:02:20 +0530

    fix: modify username rules

--- src/configs/schema.config.ts
+++ src/configs/schema.config.ts
@@ -1,13 +1,13 @@
 import Joi from "joi";
 
 const nameRules = {
-	'string.empty': "Name cannot be empty"
+	'string.empty': "Name cannot be empty",
 }
 const usernameRules = {
 	'string.empty': "Username cannot be empty"
 }
 const emailRules = {
-	'string.empty': "email cannot be empty"
+	'string.empty': "Email cannot be empty"
 }
 const passwordRules = {
 	'string.empty': "Password cannot be empty"
