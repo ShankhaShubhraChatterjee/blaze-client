1adb010c1277ca0fba762c987faa94496c1b4088
Foster Z <fosterunlimited0123@gmail.com>
2025-03-03 12:42:34 +0530

    feat(New Component): add todo-item component

---
+++ src/components/todo/todo-item/TodoItem.component.tsx
@@ -0,0 +1,53 @@
+// React Imports
+import { useState } from 'react';
+
+// Chakra Imports
+import { Button, IconButton, Stack, Input, Editable } from '@chakra-ui/react'
+import { Checkbox } from './../../ui/checkbox'
+
+// React Icon Imports
+import { FaTrashAlt, FaPenAlt, FaRegSave, FaRegStar, FaStar } from "react-icons/fa";
+
+// Stylesheet Imports
+import './todoItem.component.scss';
+
+// Root Component (TodoItem)
+const TodoItem = () => {
+    const [starred, setStarred] = useState(false)
+    const content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    return (
+        <li className='todo__preview--list__item'>
+            <header className='todo__preview--list__item--header'>
+                <div className='todo__list--item__header--section-1'>
+                    <Checkbox colorPalette='green' />
+                    <Checkbox // prettier-ignore
+                        defaultChecked={false}
+                        onChange={() => setStarred(!starred)}
+                        colorPalette='yellow'
+                        size='lg' variant='none'
+                        icon={
+                            starred ? <FaStar size={24} color='goldenrod' />
+                                :
+                                <FaRegStar size={24} color='goldenrod' />}
+                    />
+                </div>
+                <div className='todo__list--item__header--section-2'>
+                    <IconButton variant='outline'><FaRegSave color='lightblue' /></IconButton>
+                    <Button variant='outline'><FaPenAlt color='yellowgreen' /></Button>
+                    <Button variant='outline' color='red'><FaTrashAlt /></Button>
+                </div>
+            </header>
+
+            <div className='todo__list--item__content'>
+                <h2 className='todo__list--item__content--header'>Some Todo / Objective Header Title</h2>
+                <Editable.Root defaultValue={content} className='todo__list--item__content--body'>
+                    <Editable.Preview />
+                    <Editable.Textarea />
+                </Editable.Root>
+            </div>
+            <div className='todo__list--item__content--index'>100</div>
+        </li>
+    )
+}
+
+export default TodoItem;
