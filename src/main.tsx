497383d979f297b4df80552a6dfe93db2eba6f64
Foster Z <fosterunlimited0123@gmail.com>
2025-03-13 01:16:51 +0530

    feat(comment): add comments for better grouping of imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,30 @@
-// Modules
-
+// React Imports
 import React from 'react'
-import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client'
-import * as ChakraProvider from './components/ui/provider'
 
+// React Redux Imports
+import { Provider } from 'react-redux'
+
+// Chakra UI Imports
+import { Provider as ChakraProvider } from './components/ui/provider'
+
+// Custom User Imports
 import App from './App'
 
+// Stylesheet Imports
 import './main.scss'
 
-// Configs
-
+// User Configuration Imports
 import { store } from './configs/store.config'
+import { themeConfig } from './configs/theme.config'
 
+// Root Entry Point (main.tsx)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <ChakraProvider.Provider>
+    <ChakraProvider>
         <Provider store={store}>
             <React.StrictMode>
                 <App />
             </React.StrictMode>
         </Provider>
-    </ChakraProvider.Provider>
+    </ChakraProvider>
 )
