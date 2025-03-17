a15e626850191b7bb982a960bed2b2d5b316a97c
Foster Z <fosterunlimited0123@gmail.com>
2025-03-18 00:31:13 +0530

    feat(sidebar): add event listeners for toggling sidebar

--- src/pages/todo/Todo.page.tsx
+++ src/pages/todo/Todo.page.tsx
@@ -1,20 +1,32 @@
-import { useSelector } from 'react-redux'
+// React Redux Imports
+import { useDispatch, useSelector } from 'react-redux'
 
+// React Redux Slice Imports
+import { autoToggleSidebar } from './../../redux/todo-sidebar.slice'
+
+// Custom User Component Imports
 import NavBar from '../../components/navbar/Navbar.component'
 import TodoPreview from '../../components/todo/preview/TodoPreview.component'
 import TodoSideBar from '../../components/todo/sidebar/TodoSidebar.component'
 import TodoBar from '../../components/todo/toolbar/TodoToolbar.component'
+
+// Stylesheet Imports
 import './todo.page.scss'
 
+// Root Component (Todo)
 const Todo = () => {
-    const sidebarVisible = true
+    const sidebarVisible = useSelector((state: any) => state.todoSidebarSlice.sidebarVisible)
+    const dispatch = useDispatch()
+    window.addEventListener('resize', () => {
+        dispatch(autoToggleSidebar())
+    })
     return (
         <div>
             <NavBar />
             <div className="todo__container">
                 <div className="todo">
                     <TodoBar />
-                    {sidebarVisible ? <TodoSideBar /> : ''}
+                    {sidebarVisible ? <TodoSideBar translate='0px' /> : <TodoSideBar translate='-350px' />}
                     <TodoPreview />
                 </div>
             </div>
