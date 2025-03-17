b2a648ffbbb77fe60814bcf51c96b5a48db7fd63
Foster Z <fosterunlimited0123@gmail.com>
2025-03-18 00:34:15 +0530

    feat(accordion): add starred accordion & use array of objects to render items through mapping

--- src/components/todo/sidebar/TodoSidebar.component.tsx
+++ src/components/todo/sidebar/TodoSidebar.component.tsx
@@ -1,7 +1,7 @@
 // React Imports
 
 // React Icon Imports
-import { FaHeart } from 'react-icons/fa'
+import { FaRegStar } from 'react-icons/fa'
 import { GrInProgress } from 'react-icons/gr'
 import { IoCheckmarkDoneCircleSharp } from 'react-icons/io5'
 
@@ -20,47 +20,56 @@ import './todo-sidebar.component.scss'
 import { Box } from '@chakra-ui/react'
 
 const TodoSideBar = (props: any) => {
+    const sidebarAccordions = [
+        {
+            id: 1, title: 'Completed', icon: <IoCheckmarkDoneCircleSharp size={24} color="yellowgreen" />,
+            tasks: [
+                { id: 1, title: 'Objective 0' }
+            ]
+        },
+        {
+            id: 2, title: 'Remaining', icon: <GrInProgress size={24} color="goldenrod" />,
+            tasks: [
+                { id: 1, title: 'Objective 1' }
+            ]
+        },
+        {
+            id: 3, title: 'Starred', icon: <FaRegStar size={24} color="goldenrod" />,
+            tasks: [
+                { id: 1, title: 'Objective 2' },
+                { id: 2, title: 'Objective 2' }
+            ]
+        },
+    ]
     return (
         <Box
             className="todo__sidebar--container"
+            transform={`translateX(${props.translate})`}
             bg={{ base: 'white', _dark: 'black' }}
         >
             <AccordionRoot
                 variant="enclosed"
-                multiple
                 collapsible
                 defaultValue={['b']}
             >
-                {/* Completed Accordion */}
-                <AccordionItem value="completed">
-                    <AccordionItemTrigger>
-                        <IoCheckmarkDoneCircleSharp
-                            size={24}
-                            color="yellowgreen"
-                        />
-                        Completed
-                    </AccordionItemTrigger>
-                    <AccordionItemContent fontSize="14px" fontWeight="semibold">
-                        <em>Objective 0</em>
-                    </AccordionItemContent>
-                </AccordionItem>
+                {sidebarAccordions.map((accordion: any) => {
+                    return (
+                        <AccordionItem key={accordion.id} value={accordion.title}>
+                            <AccordionItemTrigger>
+                                {accordion.icon}
+                                {accordion.title}
+                            </AccordionItemTrigger>
 
-                {/* Remaining Accordion */}
-                <AccordionItem value="remaining">
-                    <AccordionItemTrigger>
-                        <GrInProgress size={24} color="goldenrod" />
-                        Remaining
-                    </AccordionItemTrigger>
-                    <AccordionItemContent fontSize="14px" fontWeight="semibold">
-                        <em>Objective 0</em>
-                    </AccordionItemContent>
-                    <AccordionItemContent fontSize="14px" fontWeight="semibold">
-                        <em>Objective 1</em>
-                    </AccordionItemContent>
-                    <AccordionItemContent fontSize="14px" fontWeight="semibold">
-                        <em>Objective 2</em>
-                    </AccordionItemContent>
-                </AccordionItem>
+                            {accordion.tasks.map((task: any) => {
+                                return (
+                                    <AccordionItemContent fontSize="14px" fontWeight="semibold">
+                                        <em key={task.id}>{task.title}</em>
+                                    </AccordionItemContent>
+                                )
+                            })}
+                        </AccordionItem>
+                    )
+                })}
             </AccordionRoot>
         </Box>
     )
