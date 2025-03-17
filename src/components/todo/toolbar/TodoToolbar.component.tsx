b48ccfaa6e80c32796d5b8db089e0b3564cd4641
Foster Z <fosterunlimited0123@gmail.com>
2025-03-18 00:21:51 +0530

    feat(button): add sidebar toggle button

--- src/components/todo/toolbar/TodoToolbar.component.tsx
+++ src/components/todo/toolbar/TodoToolbar.component.tsx
@@ -1,5 +1,11 @@
 // React Icons Imports
 import { FaPlus, FaTrash } from 'react-icons/fa6'
+import { FiSidebar } from "react-icons/fi";
+
+// Redux Imports
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleSideBar } from './../../../redux/todo-sidebar.slice';
+
 // Chakra UI Imports
 import { Box, Button, Stack, Text } from '@chakra-ui/react'
 
@@ -8,8 +14,10 @@ import './todo-toolbar.component.scss'
 
 // Root Component (TodoBar)
 const TodoBar = () => {
-    const createTodoItem = () => {}
-    const deleteTodoItem = () => {}
+    const dispatch = useDispatch()
+    const sidebarVisible: boolean = useSelector((state: any) => state.todoSidebarSlice.sidebarVisible)
+    const createTodoItem = () => { }
+    const deleteTodoItem = () => { }
     return (
         <div className="todo__toolbar">
             <Stack
@@ -18,13 +26,23 @@ const TodoBar = () => {
                 width="full"
                 justifyContent="space-between"
             >
-                <Text className="todo__toolbar--header" fontSize="xl">
+                <Text className="todo__toolbar--header" fontSize="md">
                     User's Todo Dashboard
                 </Text>
                 <Box className="todo__toolbar--button--container">
                     <Button
+                        className="todo__toolbar--button todo__toolbar--sidebar--toggle--button"
+                        size="xs"
+                        colorPalette="blue"
+                        rounded="full"
+                        marginRight="1.5"
+                        onClick={() => dispatch(toggleSideBar(!sidebarVisible))}
+                    >
+                        <FiSidebar />
+                    </Button>
+                    <Button
                         className="todo__toolbar--button"
-                        size="sm"
+                        size="xs"
                         colorPalette="green"
                         rounded="full"
                         marginRight="1.5"
@@ -34,7 +52,7 @@ const TodoBar = () => {
                     </Button>
                     <Button
                         className="todo__toolbar--button"
-                        size="sm"
+                        size="xs"
                         colorPalette="red"
                         rounded="full"
                     >
