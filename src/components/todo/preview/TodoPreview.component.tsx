31ef245bce76ce3f6f79d99c0585c159c5812fb4
Foster Z <fosterunlimited0123@gmail.com>
2025-03-18 00:47:30 +0530

    feat(drawer): add drawer to preview component

--- src/components/todo/preview/TodoPreview.component.tsx
+++ src/components/todo/preview/TodoPreview.component.tsx
@@ -1,7 +1,7 @@
 // React Imports
 
 // Chakra UI Imports
-import { Box } from '@chakra-ui/react'
+import { Box, Button, CloseButton, Drawer, Portal } from '@chakra-ui/react'
 
 // Custom User Component Imports
 import TodoItem from '../todo-item/TodoItem.component'
@@ -20,6 +20,35 @@ const TodoPreview = () => {
                 Todos / Objectives
             </Box>
             <ul className="todo__preview--list">
+                <Drawer.Root>
+                    <Drawer.Trigger asChild>
+                        <Button variant="outline" size="sm">
+                            Open Drawer
+                        </Button>
+                    </Drawer.Trigger>
+                    <Portal>
+                        <Drawer.Backdrop />
+                        <Drawer.Positioner>
+                            <Drawer.Content>
+                                <Drawer.Header>
+                                    <Drawer.Title>Drawer Title</Drawer.Title>
+                                </Drawer.Header>
+                                <Drawer.Body>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    </p>
+                                </Drawer.Body>
+                                <Drawer.Footer>
+                                    <Button>Save</Button>
+                                </Drawer.Footer>
+                                <Drawer.CloseTrigger asChild>
+                                    <CloseButton size="sm" />
+                                </Drawer.CloseTrigger>
+                            </Drawer.Content>
+                        </Drawer.Positioner>
+                    </Portal>
+                </Drawer.Root>
                 {todos.map((todo: any, index: number) => {
                     return (
                         <TodoItem
