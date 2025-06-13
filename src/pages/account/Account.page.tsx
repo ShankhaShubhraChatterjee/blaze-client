f2ec9b4425a5ec119316d3a05f1845f3e856ca1e
Foster Z <fosterunlimited0123@gmail.com>
2025-06-13 16:57:47 +0530

    feat(array): use arrays of object to render elements through mapping

--- src/pages/account/Account.page.tsx
+++ src/pages/account/Account.page.tsx
@@ -1,20 +1,20 @@
 // Chakra Imports
-import { Link as ChakraLink, Tabs } from '@chakra-ui/react'
+import { Tabs, Link as ChakraLink } from '@chakra-ui/react'
+import { Link, useNavigate } from 'react-router-dom'
 import { Tooltip } from '../../components/ui/tooltip'
 
 // Custom User Imports
 import NavBar from '../../components/navbar/Navbar.component'
-import AccountIntro from '../../components/account/intro/AccountIntro.component'
+import AccountIntro from '../../components/account/overview/AccountOverview.component'
 import AccountUpdate from '../../components/account/update/AccountUpdate.component'
 import AccountEmail from '../../components/account/email/AccountEmail.component'
 import PasswordUpdate from '../../components/account/password/AccountPassword.component'
-import AccountAdditional from '../../components/account/additional/AccountAdditional.component'
 import AccountDelete from '../../components/account/delete/AccountDelete.component'
 import AccountMFA from './../../components/account/mfa/AccountMFA.component'
 
 // React Icons Imports
 import { RiEditBoxFill, RiProfileLine } from 'react-icons/ri'
-import { FaPlus, FaEnvelope } from 'react-icons/fa'
+import { FaEnvelope } from 'react-icons/fa'
 import { CgDanger } from 'react-icons/cg'
 import { TbPasswordUser, TbShieldLock } from 'react-icons/tb'
 
@@ -23,37 +23,35 @@ import './account.page.scss'
 
 // Root Component (Account)
 const Account = () => {
+    const navigate = useNavigate()
     const tabs = [
-        { id: 1, title: 'Overview', value: 'overview', icon: <RiProfileLine size={20} /> },
-        { id: 2, title: 'Update', value: 'update', icon: <RiEditBoxFill size={20} /> },
-        { id: 3, title: 'Update Email', value: 'update-email', icon: <FaEnvelope size={20} /> },
-        { id: 4, title: 'Update Password', value: 'update-password', icon: <TbPasswordUser size={24} /> },
-        { id: 5, title: 'MFA', value: 'mfa', icon: <TbShieldLock size={20} /> },
-        // { id: 6, title: 'Additional', value: 'additional', icon: <FaPlus size={20} /> },
-        { id: 7, title: 'Danger', value: 'danger', icon: <CgDanger size={20} /> },
+        { id: 1, title: 'Overview', value: 'overview', url: 'user/overview', icon: <RiProfileLine size={20} /> },
+        { id: 2, title: 'Update Profile', value: 'update-profile', url: 'user/update/profile', icon: <RiEditBoxFill size={20} /> },
+        { id: 3, title: 'Update Email', value: 'update-email', url: 'user/update/email', icon: <FaEnvelope size={20} /> },
+        { id: 4, title: 'Update Password', value: 'update-password', url: 'user/update/password', icon: <TbPasswordUser size={24} /> },
+        { id: 5, title: 'MFA', value: 'mfa', url: 'user/mfa', icon: <TbShieldLock size={20} /> },
+        { id: 6, title: 'Danger', value: 'danger', url: 'user/danger', icon: <CgDanger size={20} /> },
     ]
     const tabContents = [
         { id: 1, value: 'overview', component: <AccountIntro /> },
-        { id: 2, value: 'update', component: <AccountUpdate /> },
+        { id: 2, value: 'update-profile', component: <AccountUpdate /> },
         { id: 3, value: 'update-email', component: <AccountEmail /> },
         { id: 4, value: 'update-password', component: <PasswordUpdate /> },
         { id: 5, value: 'mfa', component: <AccountMFA /> },
-        // { id: 6, value: 'additional', component: <AccountAdditional /> },
-        { id: 7, value: 'danger', component: <AccountDelete /> },
+        { id: 6, value: 'danger', component: <AccountDelete /> },
     ]
     return (
         <div>
             <NavBar />
             <div className="account__page">
-                <Tabs.Root defaultValue="overview" fitted>
+                <Tabs.Root defaultValue="overview" navigate={({ value, node }) => navigate(`/${value}`)} fitted>
                     <Tabs.List>
                         {tabs.map((tab) => {
                             return (
                                 <Tabs.Trigger value={tab.value} key={tab.id}>
                                     <Tooltip content={tab.title} openDelay={50} closeDelay={50}>
                                         <ChakraLink
-                                            unstyled
-                                            href="#"
+                                            href={`/account/${tab.url}`}
                                             className="account__page--tab--item"
                                         >
                                             {tab.icon}
