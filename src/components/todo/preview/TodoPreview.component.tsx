a620ee92b2c66c5689c33d3dc002710f0fcf6ac6
Foster Z <fosterunlimited0123@gmail.com>
2025-06-13 17:45:58 +0530

    feat(layout): redesign the forms

--- src/components/todo/preview/TodoPreview.component.tsx
+++ src/components/todo/preview/TodoPreview.component.tsx
@@ -1,7 +1,9 @@
-// React Imports
+// React DnD Imports
+import { DndContext, closestCorners } from '@dnd-kit/core'
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 
 // Chakra UI Imports
-import { Box, Button, CloseButton, Drawer, Portal } from '@chakra-ui/react'
+import { Box, Button, CloseButton, Drawer, Editable, IconButton, Portal } from '@chakra-ui/react'
 
 // Custom User Component Imports
 import TodoItem from '../todo-item/TodoItem.component'
@@ -11,16 +13,20 @@ import './todo-preview.component.scss'
 
 // Temporary Data Imports
 import { todos } from '../../../../tmp/data/data'
+import { Checkbox } from '../../ui/checkbox'
+import { LuPencilLine, LuX, LuCheck } from 'react-icons/lu'
 
 // Root Component (TodoPreview)
 const TodoPreview = () => {
+    const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed doeiusmod tempor incididunt ut labore et dolore magna aliqua."
+
     return (
         <div className="todo__preview--container">
             <Box shadow="md" className="todo__preview--header">
                 Todos / Objectives
             </Box>
             <ul className="todo__preview--list">
-                <Drawer.Root>
+                <Drawer.Root closeOnInteractOutside={false}>
                     <Drawer.Trigger asChild>
                         <Button variant="outline" size="sm">
                             Open Drawer
@@ -31,13 +37,37 @@ const TodoPreview = () => {
                         <Drawer.Positioner>
                             <Drawer.Content>
                                 <Drawer.Header>
-                                    <Drawer.Title>Drawer Title</Drawer.Title>
+
+                                    <Checkbox colorPalette='purple' marginBottom={1} />
+                                    <Drawer.Title display='flex' alignItems='flex-start'>
+                                        <Editable.Root defaultValue="Lorem ipsum dolor sit amet, consectetur sit amet, consectetur adipiscing">
+                                            <Editable.Preview />
+                                            <Editable.Input />
+                                            <Editable.Control>
+                                                <Editable.EditTrigger asChild>
+                                                    <IconButton variant="ghost" size="xs">
+                                                        <LuPencilLine />
+                                                    </IconButton>
+                                                </Editable.EditTrigger>
+                                                <Editable.CancelTrigger asChild>
+                                                    <IconButton variant="outline" size="xs">
+                                                        <LuX />
+                                                    </IconButton>
+                                                </Editable.CancelTrigger>
+                                                <Editable.SubmitTrigger asChild>
+                                                    <IconButton variant="outline" size="xs">
+                                                        <LuCheck />
+                                                    </IconButton>
+                                                </Editable.SubmitTrigger>
+                                            </Editable.Control>
+                                        </Editable.Root>
+                                    </Drawer.Title>
                                 </Drawer.Header>
                                 <Drawer.Body>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                                        eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                    </p>
+                                    <Editable.Root defaultValue={content}>
+                                        <Editable.Preview minH="auto" alignItems="flex-start" width="full" />
+                                        <Editable.Textarea minHeight='350px' maxLength={1000} />
+                                    </Editable.Root>
                                 </Drawer.Body>
                                 <Drawer.Footer>
                                     <Button>Save</Button>
@@ -49,15 +79,22 @@ const TodoPreview = () => {
                         </Drawer.Positioner>
                     </Portal>
                 </Drawer.Root>
-                {todos.map((todo: any, index: number) => {
-                    return (
-                        <TodoItem
-                            key={index}
-                            title={todo.title}
-                            description={todo.description}
-                        />
-                    )
-                })}
+                <DndContext collisionDetection={closestCorners}>
+                    <SortableContext items={todos} strategy={verticalListSortingStrategy}>
+                        {todos.map((todo: any, index: number) => {
+                            return (
+
+                                <TodoItem
+                                    key={index}
+                                    id={index}
+                                    title={todo.title}
+                                    description={todo.description}
+                                />
+
+                            )
+                        })}
+                    </SortableContext>
+                </DndContext>
             </ul>
         </div>
     )
