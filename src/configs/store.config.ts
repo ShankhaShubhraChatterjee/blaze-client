7a14081d323d99c1e8309c6555f986c22d9741ea
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmail.com>
2026-02-03 01:06:13 +0530

    feat: add todo slice for handling todo list operations

--- src/configs/store.config.ts
+++ src/configs/store.config.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+
 import todoSidebarSlice from '../redux/todo-sidebar.slice'
 import authSlice from '../redux/auth.slice'
+import todoSlice from '../redux/todo.slice';
 
 export const store = configureStore({
     reducer: {
         todoSidebarSlice: todoSidebarSlice,
         authSlice: authSlice,
+        todoSlice: todoSlice
     },
 })
---
+++ src/redux/todo.slice.ts
@@ -0,0 +1,74 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { arrayMove } from "@dnd-kit/sortable";
+
+const initialState = {
+    todos: [
+        {
+            id: 1,
+            title: 'Do Not Do Anything',
+            index: 2,
+            starred: true,
+            checked: false,
+            description:
+                'Task-1 it seems like this todo is wayy much larger and longer than i expected it to be',
+        },
+        {
+            id: 2,
+            index: 3,
+            starred: false,
+            checked: true,
+            title: 'Do Something Crazy As Hell',
+            description: 'Task-2 Is Somewhat Smaller Comparatively',
+        },
+        // {
+        //     id: 3,
+        //     index: 4,
+        //     starred: false,
+        //     checked: false,
+        //     title: 'Do Nothing At All',
+        //     description: 'Task 4 Is Biggg !!!',
+        // },
+        // {
+        //     id: 4,
+        //     index: 5,
+        //     starred: false,
+        //     checked: false,
+        //     title: 'Do Whatever You Want',
+        //     description: 'Task 5 Is Tiny & Random',
+        // },
+        // {
+        //     id: 5,
+        //     index: 1,
+        //     starred: false,
+        //     checked: false,
+        //     title: 'Do Nothing At All',
+        //     description: 'Task 4 Is Biggg !!!',
+        // },
+    ]
+}
+
+
+
+const todoSlice = createSlice({
+    name: 'todoSlice',
+    initialState,
+    reducers: {
+        addTodo: (state: any) => {
+            state.todos = [...state.todos, {
+                id: state.todos.length + 1,
+                title: 'Change This Title For Header ...',
+                index: state.todos.length + 1,
+                starred: false,
+                checked: false,
+                description: 'Describe Your Task Here Briefly ...',
+            }]
+        },
+        sortTodo: (state: any, action) => {
+            console.log("Working")
+            state.todos = action.payload
+        }
+    },
+})
+
+export const { addTodo, sortTodo } = todoSlice.actions;
+export default todoSlice.reducer;
