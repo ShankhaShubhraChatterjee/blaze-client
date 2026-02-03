e35d19813307f49755db08d37fe159561638ed20
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmail.com>
2026-02-03 16:02:34 +0530

    feat: change footer name and add a link

--- src/components/footer/Footer.component.tsx
+++ src/components/footer/Footer.component.tsx
@@ -1,10 +1,14 @@
 // React Icons Imports
 import { FaFacebook, FaInstagram, FaXTwitter } from 'react-icons/fa6'
 
+import { Link } from '@chakra-ui/react'
+
 import './footer.component.scss'
+
 
 const Footer = () => {
     const date = new Date().getFullYear()
+    const name = "S.S. Chatterjee"
     return (
         <footer className="footer">
             <div className="footer__social">
@@ -19,7 +23,7 @@ const Footer = () => {
                 </a>
             </div>
             <p className="footer__copyright">
-                &copy; Copyright {date}, Foster Z
+                &copy; Copyright {date}, <Link href="#" colorPalette="teal">{name}</Link>
             </p>
         </footer>
     )
